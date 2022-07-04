62f8ce25c6fa1c9ae67b818e3e90bc9a365df023
Mijndert Stuij <mijndert@mijndertstuij.nl>
2022-07-04 08:41:19 +0200

    change repo uri for openttd

--- openttd/run-openttd.sh
+++ openttd/run-openttd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name openttd -e LOCAL_USER_ID=`id -u $USER` -p 3979:3979/tcp -p 3979:3979/udp --restart=always --log-opt max-size=50m -v ~/openttd-data:/config:rw mijndert/openttd-docker:latest
+docker run -d --name openttd -e LOCAL_USER_ID=`id -u $USER` -p 3979:3979/tcp -p 3979:3979/udp --restart=always --log-opt max-size=50m -v ~/openttd-data:/config:rw ghcr.io/mijndert/openttd:latest
